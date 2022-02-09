0fe5b73709e1c353fcd602f571b2d1b7ff3c1dbf
JS @breaktoprotect <breaktoprotect@gmail.com>
2022-02-09 11:30:57 +0800

    Updated react-router-dom to v6 and relevant code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Dashboard from "./components/dashboard";
 import ErrorPage from "./components/errorpage";
@@ -11,12 +11,12 @@ function App() {
         <div className="App">
             <NavBar />
             <div className="content">
-                <Switch>
-                    <Route path="/upload" component={UploadForm} />
-                    <Route path="/error404" component={ErrorPage} />
-                    <Route path="/" exact component={Dashboard} />
-                    <Redirect to="/error404" />
-                </Switch>
+                <Routes>
+                    <Route path="/upload" element={<UploadForm />} />
+                    <Route path="/error404" element={<ErrorPage />} />
+                    <Route path="/" exact element={<Dashboard />} />
+                    <Route path="*" element={<ErrorPage />} />
+                </Routes>
             </div>
         </div>
     );
